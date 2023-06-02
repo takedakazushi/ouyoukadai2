526aef42049f1ccf27b0525016e5771eea2ff64e
takedakazushi <ec2-user@ip-172-31-44-28.ap-northeast-1.compute.internal>
2023-06-02 11:27:43 +0000

    ouyoukadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- has_many :books, dependent: :destroy
-  belongs_to :books
+ has_many :books
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
-  end
-  profile_image.variant(resize_to_limit: [width, height]).processed
-end
+  validates :introduction, length: {maximum: 50}
+
   
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
