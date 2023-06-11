768ac4d76487e3a6fa9d1ca60a72094e68d154c0
takedakazushi <ec2-user@ip-172-31-44-28.ap-northeast-1.compute.internal>
2023-06-11 09:45:19 +0000

    ouyoukadai6

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
 end
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :users do
+  resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
